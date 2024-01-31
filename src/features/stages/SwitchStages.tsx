5fb4c1e68f2d9ffb32902a28a2e154d8e36243f7
MacJ13 <jacdim02@gmail.com>
2024-02-01 00:37:46 +0100

    Change pomodoro stage by stage buttons

--- src/features/stages/SwitchStages.tsx
+++ src/features/stages/SwitchStages.tsx
@@ -1,18 +1,37 @@
 import { useSelector } from "react-redux";
 import { selectAllStages } from "../../redux/stagesSlice";
 import styled from "styled-components";
+import { useDispatch } from "react-redux";
+import { changeStageId, selectPomodoroId } from "../../redux/pomodoroSlice";
+import { StageId } from "../../types/types";
 
 const SwitchStages = () => {
   const stages = useSelector(selectAllStages);
 
+  const currentId = useSelector(selectPomodoroId);
+
+  const dispatch = useDispatch();
+
+  const changeStage = (id: StageId) => {
+    dispatch(changeStageId(id));
+  };
+
   const buttonStages = stages.map((stage) => (
-    <Button key={stage.id}>{stage.name}</Button>
+    <Button
+      active={currentId === stage.id}
+      key={stage.id}
+      onClick={() => {
+        changeStage(stage.id);
+      }}
+    >
+      {stage.name}
+    </Button>
   ));
 
-  return <Wrapper>{buttonStages}</Wrapper>;
+  return <Stages>{buttonStages}</Stages>;
 };
 
-const Wrapper = styled.div`
+const Stages = styled.div`
   position: relative;
   display: flex;
   gap: 1rem;
@@ -21,12 +40,12 @@ const Wrapper = styled.div`
   margin: 1rem 0;
 `;
 
-const Button = styled.button`
+const Button = styled.button<{ active: boolean }>`
   width: 7.5rem;
   border-radius: 0.375rem;
   padding: 0.5rem 0;
   background-color: rgba(255, 255, 255, 0.25);
-  font-weight: 600;
+  font-weight: ${(props) => (props.active ? 600 : 500)};
   color: #fff;
 `;
 
