b5166d8c6c96ff53a4ea71c748425e7be8273b96
MacJ13 <jacdim02@gmail.com>
2024-02-07 11:28:00 +0100

    Add sounds file for countdown alert

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,3 +1,12 @@
+export const SOUNDS = Object.keys(import.meta.glob("../assets/audio/*")).map(
+  (sound) => {
+    const regex = /\//gi;
+    const name = sound.replace(regex, ".").split(".").slice(-2).shift();
+
+    return { src: sound, name };
+  }
+);
+
 export const THEMES: string[] = [
   "#c65151",
   "#258749",
