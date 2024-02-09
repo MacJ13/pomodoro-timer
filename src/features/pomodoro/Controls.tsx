a7ad6e35dc91e0b43fb15d1ad485ff9e3e03fbb9
MacJ13 <jacdim02@gmail.com>
2024-02-09 23:01:28 +0100

    Play 'switch' sound clicking start/pause button

--- src/features/pomodoro/Controls.tsx
+++ src/features/pomodoro/Controls.tsx
@@ -6,6 +6,9 @@ import RepeatSvg from "../../assets/icons/repeat.svg?react";
 import NextSvg from "../../assets/icons/next.svg?react";
 
 import { useDispatch, useSelector } from "react-redux";
+
+import SWITCH_SOUND from "src/assets/audio/switch.mp3";
+
 import {
   changeNextStage,
   changeStatus,
@@ -13,6 +16,7 @@ import {
 } from "../../redux/pomodoroSlice";
 
 import { stopSound } from "../../redux/soundSlice";
+import { useRef } from "react";
 
 type ControlsProps = {
   repeat: () => void;
@@ -25,6 +29,13 @@ const Controls = ({ repeat }: ControlsProps) => {
 
   const isStart = status === "start";
 
+  const audioRef = useRef<HTMLAudioElement>(new Audio(SWITCH_SOUND));
+
+  const clickSound = () => {
+    audioRef.current.currentTime = 0;
+    audioRef.current.play();
+  };
+
   return (
     <Bar>
       <Button disabled={!isStart} onClick={repeat}>
@@ -35,6 +46,8 @@ const Controls = ({ repeat }: ControlsProps) => {
         onClick={() => {
           dispatch(changeStatus());
           dispatch(stopSound());
+          clickSound();
+          // isStart ? dispatch(stopSound()) : dispatch(playSwitchSound());
         }}
       >
         {isStart ? <PauseSvg /> : <PlaySvg />}
