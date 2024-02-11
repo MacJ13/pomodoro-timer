0cf0c81eead5a714279ece87e0a4ed2c76da35e3
MacJ13 <jacdim02@gmail.com>
2024-02-11 14:53:03 +0100

    Update Update Navbar component

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -2,78 +2,50 @@ import { useDispatch } from "react-redux";
 import Settings from "../../assets/icons/settings.svg?react";
 import styled from "styled-components";
 import { toggleSettings } from "../../redux/settingsSlice";
+import { Button } from "../styles/Button.styled";
+import { Flex } from "../styles/Flex.styled";
+import { Icon } from "../styles/Icon.styled";
 
 const Navbar = () => {
   const dispatch = useDispatch();
 
   return (
     <Nav>
-      <Title>Pomodoro timer</Title>
-      <div>
-        <Button onClick={() => dispatch(toggleSettings())}>
-          <SvgWrapper>
-            <Settings />
-          </SvgWrapper>
-          <ButtonText>Settings</ButtonText>
-        </Button>
-      </div>
+      <FlexNav>
+        <h2>Pomodoro timer</h2>
+        <ButtonNav onClick={() => dispatch(toggleSettings())}>
+          <Flex>
+            <Icon $size="1rem">
+              <Settings />
+            </Icon>
+            Settings
+          </Flex>
+        </ButtonNav>
+      </FlexNav>
     </Nav>
   );
 };
 
 const Nav = styled.nav`
-  align-items: center;
-  display: flex;
-  font-size: 1rem;
-  justify-content: space-between;
-
   margin: 0 auto;
   max-width: 30rem;
   padding: 0 1rem;
   width: 100%;
 `;
 
-const Title = styled.h2`
-  font-size: 1.25rem;
-`;
-
-const Button = styled.button`
-  background-color: transparent;
-  border: ${({ theme }) => theme.borders.slimTranslucent};
-  border-radius: 0.5rem;
-
-  align-items: center;
-  display: flex;
-  gap: 0.5rem;
+const FlexNav = styled(Flex)`
   justify-content: space-between;
+`;
 
+const ButtonNav = styled(Button)`
+  // border: ${({ theme }) => theme.borders.slimTranslucent};
   padding: 0.5rem 1.125rem;
+  font-size: 0.8rem;
+  border: none;
 
-  transition: background-color 0.25s;
-
-  &:hover {
-    background-color: ${({ theme }) => theme.colors.white025};
+  & > div {
+    gap: 0.5rem;
   }
-
-  &:active {
-    transform: translateY(2px);
-  }
-`;
-
-const SvgWrapper = styled.div`
-  width: 1rem;
-  height: 1rem;
-
-  & > svg {
-    display: block;
-  }
-`;
-
-const ButtonText = styled.span`
-  color: #fff;
-  font-size: 0.775rem;
-  font-weight: 500;
-  letter-spacing: 1px;
 `;
 
 export default Navbar;
