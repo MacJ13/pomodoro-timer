a0873b94635258b4857a5e639e9bde846ef32e11
MacJ13 <jacdim02@gmail.com>
2024-02-11 20:57:24 +0100

    Fix problem with toggle autostart buttons

--- src/features/settings/ToggleButton.tsx
+++ src/features/settings/ToggleButton.tsx
@@ -7,34 +7,34 @@ const ToggleButton = ({ active, onClick }: ToggleButtonProps) => {
 
   return (
     <Button
-      active={toggle}
+      $active={toggle}
       onClick={() => {
         setToggle(!toggle);
         onClick(!toggle);
       }}
     >
-      <Switch active={toggle} />
+      <Switch $active={toggle} />
     </Button>
   );
 };
 
-const Button = styled.button<{ active: boolean }>`
+const Button = styled.button<{ $active: boolean }>`
   position: relative;
   width: 4rem;
   height: 2.2rem;
 
   border-radius: 1.5rem;
-  background-color: ${(props) =>
-    !props.active ? "rgb(241, 241, 241)" : "#81ea44"};
+  background-color: ${({ $active }) =>
+    !$active ? "rgb(241, 241, 241)" : "#81ea44"};
 `;
 
-const Switch = styled.div<{ active: boolean }>`
+const Switch = styled.div<{ $active: boolean }>`
   position: absolute;
   width: calc(2.2rem - 0.4rem);
   height: calc(2.2rem - 0.4rem);
   top: 0.2rem;
-  left: ${(props) =>
-    !props.active ? "0.2rem" : "calc(100% - 2.2rem + 0.2rem)"};
+  left: ${({ $active }) =>
+    !$active ? "0.2rem" : "calc(100% - 2.2rem + 0.2rem)"};
 
   background-color: white;
   border-radius: 50%;
