dc05c57ab36bf8fe79a0285b657d09e874017745
MacJ13 <jacdim02@gmail.com>
2024-02-11 20:48:46 +0100

    Modify Settings row component

--- src/features/settings/SettingsRow.tsx
+++ src/features/settings/SettingsRow.tsx
@@ -1,26 +1,26 @@
 import styled from "styled-components";
-
 import { RowProps } from "../../types/types";
+import { Flex } from "src/components/styles/Flex.styled";
 
 const SettingsRow = ({ title, children }: RowProps) => {
   return (
     <Row>
-      <RowTitle>{title}</RowTitle>
-      {children}
+      <Flex $align="flex-start" $justify="space-between">
+        <Title>{title}</Title>
+        {children}
+      </Flex>
     </Row>
   );
 };
 
-const RowTitle = styled.h2`
+const Title = styled.h3`
   color: rgba(0, 0, 0, 0.375);
-  font-size: 1rem;
+  font-size: 1.05rem;
   font-weight: 600;
   letter-spacing: 1px;
 `;
 
 const Row = styled.div`
-  display: flex;
-  justify-content: space-between;
   margin-bottom: 1.5rem;
   padding-bottom: 1rem;
   border-bottom: 1px solid rgba(0, 0, 0, 0.125);
