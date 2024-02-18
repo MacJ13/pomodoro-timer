9c51b5143a167e8ab3f4c058413b077b2e7a74f0
MacJ13 <jacdim02@gmail.com>
2024-02-18 19:43:39 +0100

    Update close outside sound list component

--- src/features/sound/SoundList.tsx
+++ src/features/sound/SoundList.tsx
@@ -1,16 +1,28 @@
 import styled from "styled-components";
 import { SOUNDS } from "../../constants/constants";
 import DropdownSvg from "../../assets/icons/dropdown.svg?react";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { capitalize, getFilename } from "../../helpers/helpers";
 import { useDispatch, useSelector } from "react-redux";
 import { changeSource, getSource } from "../../redux/soundSlice";
 import { Flex } from "src/components/styles/Flex.styled";
+import { List } from "src/components/styles/List.styled";
 
 const SoundList = () => {
   const source = useSelector(getSource);
   const dispatch = useDispatch();
   const [open, setOpen] = useState(false);
+
+  const dropdownRef = useRef<HTMLUListElement>(null);
+
+  const handleOutsideClick = (e: MouseEvent) => {
+    if (
+      dropdownRef.current &&
+      !dropdownRef.current.contains(e.target as Node)
+    ) {
+      setOpen(false);
+    }
+  };
 
   const changeSoundAlert = (name: string) => {
     dispatch(changeSource(name));
@@ -34,19 +46,38 @@ const SoundList = () => {
 
   const currentSoundName = capitalize(getFilename(source));
 
-  const toggleSoundList = () => {
+  const toggleSoundList = (e: React.MouseEvent<HTMLElement>) => {
+    stopPropagation(e);
     setOpen(!open);
   };
 
-  const stopPropagation = (e: React.MouseEvent<HTMLUListElement>) => {
+  const stopPropagation = (e: React.MouseEvent<HTMLElement>) => {
     e.stopPropagation();
   };
+
+  useEffect(() => {
+    if (open)
+      document
+        .getElementById("modal-wrapper")
+        ?.addEventListener("click", handleOutsideClick);
+
+    return () => {
+      if (open)
+        document
+          .getElementById("modal-wrapper")
+          ?.removeEventListener("click", handleOutsideClick);
+    };
+  });
 
   return (
     <Dropdown $open={open} onClick={toggleSoundList}>
       <Flex $justify="space-between">
         {currentSoundName} <DropdownSvg />
-        {open && <List onClick={stopPropagation}>{soundItems}</List>}
+        {open && (
+          <DropdownList ref={dropdownRef} onClick={stopPropagation}>
+            {soundItems}
+          </DropdownList>
+        )}
       </Flex>
     </Dropdown>
   );
@@ -68,45 +99,14 @@ const Dropdown = styled.div<{ $open: boolean }>`
     padding: 0.5rem 1rem;
     width: 100%;
   }
-
-  ${(props) =>
-    props.$open
-      ? `
-    z-index: 0;
-    &:before {
-  
-      content: "";
-      position: fixed;
-      height: 100%;
-      width: 100%;
-      left: 0;
-      top: 0;
-      cursor: auto;
-      z-index: -1;
-    }
-    `
-      : ""}
 `;
 
-const List = styled.ul`
-  background-color: ${({ theme }) => theme.colors.white};
-  position: absolute;
-  list-style: none;
-  top: 100%;
+const DropdownList = styled(List)`
+  color: rgba(0, 0, 0, 0.5);
   left: 0;
-  border-radius: 0.33rem;
-
-  width: 100%;
-  padding: 0.5rem 0;
-  box-shadow: rgba(0, 0, 0, 0.12) 0px 1px 3px, rgba(0, 0, 0, 0.24) 0px 1px 2px;
-  z-index: 0;
 `;
 
 const Item = styled.li`
-  padding: 0 0.5rem;
-
-  color: rgba(0, 0, 0, 0.5);
-
   &:hover {
     background-color: ${({ theme }) => theme.colors.black025};
     color: ${({ theme }) => theme.colors.white};
@@ -122,6 +122,50 @@ const Button = styled.button`
   font-family: inherit;
   font-weight: 600;
   text-align: left;
+  padding: 0 0.75rem;
 `;
+
+// ${(props) =>
+//   props.$open
+//   ? `
+// z-index: 0;
+// &:before {
+
+// content: "";
+// position: fixed;
+// height: 100%;
+// width: 100%;
+// left: 0;
+// top: 0;
+// cursor: auto;
+// z-index: -1;
+// }
+// `
+//   : ""}
+
+// const List = styled.ul`
+//   background-color: ${({ theme }) => theme.colors.white};
+//   position: absolute;
+//   list-style: none;
+//   top: 100%;
+//   left: 0;
+//   border-radius: 0.33rem;
+
+//   width: 100%;
+//   padding: 0.5rem 0;
+//   box-shadow: rgba(0, 0, 0, 0.12) 0px 1px 3px, rgba(0, 0, 0, 0.24) 0px 1px 2px;
+//   z-index: 0;
+// `;
+
+// const Item = styled.li`
+//   padding: 0 0.5rem;
+
+//   color: rgba(0, 0, 0, 0.5);
+
+//   &:hover {
+//     background-color: ${({ theme }) => theme.colors.black025};
+//     color: ${({ theme }) => theme.colors.white};
+//   }
+// `;
 
 export default SoundList;
