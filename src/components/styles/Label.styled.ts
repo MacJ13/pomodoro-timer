d46c540d58a3ae877a48e18bf8a8db100f6015b7
MacJ13 <jacdim02@gmail.com>
2024-02-18 13:48:43 +0100

    Add optional width to Label styled component

--- src/components/styles/Label.styled.ts
+++ src/components/styles/Label.styled.ts
@@ -1,7 +1,8 @@
 import styled from "styled-components";
 
-export const Label = styled.label`
+export const Label = styled.label<{ $width?: string }>`
   color: ${({ theme }) => theme.colors.black0375};
   font-weight: 600;
   font-size: 0.9rem;
+  width: ${(props) => props.$width || "auto"};
 `;
