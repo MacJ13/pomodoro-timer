cb2e4686c380f453fb595ef411827d50340cbcf9
MacJ13 <jacdim02@gmail.com>
2024-02-21 20:57:24 +0100

    Update CircularProgress style and animation

--- src/features/pomodoro/CircularProgress.tsx
+++ src/features/pomodoro/CircularProgress.tsx
@@ -12,26 +12,28 @@ type CircularProgressProps = {
 const CircularProgress = ({ stage, size, current }: CircularProgressProps) => {
   const status = useSelector(getStatus);
 
-  const duration = status === "start" ? current : stage.duration;
+  const duration = current;
 
   const hiddenAnimation = current === stage.duration && document.hidden;
 
   const strokeWidth = 8;
 
-  const circleRadius = size / 2 - strokeWidth + 2;
+  const circleRadius = size / 2 - strokeWidth + 5;
   const difference = (duration / stage.duration) * 100;
   const offsetProgress = 100 - difference;
   const circumreference = 2 * Math.PI * circleRadius;
   const offset = circumreference * ((100 - offsetProgress) / 100);
 
   return (
-    <Wrapper $status={status} $hidden={hiddenAnimation}>
+    <Wrapper $duration={duration} $status={status} $hidden={hiddenAnimation}>
       <svg
         width={size}
         height={size}
         viewBox={`0 0 ${size} ${size}`}
         style={{
           transform: "rotate(-90deg)",
+          boxShadow: "-2px 0px 0px 2px rgba(255, 255, 255, 0.125)",
+          borderRadius: "50%",
         }}
       >
         <circle
@@ -45,6 +47,7 @@ const CircularProgress = ({ stage, size, current }: CircularProgressProps) => {
           strokeWidth={strokeWidth}
         ></circle>
         <circle
+          id="circle2"
           r={circleRadius}
           cx="50%"
           cy="50%"
@@ -53,7 +56,7 @@ const CircularProgress = ({ stage, size, current }: CircularProgressProps) => {
           stroke="#ffffff"
           // opacity={0.5}
           // stroke={stage.color}
-          strokeLinecap="round"
+          // strokeLinecap="round"
           strokeWidth={strokeWidth}
           strokeDasharray={`${circumreference}px`}
           strokeDashoffset={`${offset}px`}
@@ -63,13 +66,33 @@ const CircularProgress = ({ stage, size, current }: CircularProgressProps) => {
   );
 };
 
-const Wrapper = styled.div<{ $status: string; $hidden: boolean }>`
+const Wrapper = styled.div<{
+  $status: string;
+  $duration: number;
+  $hidden: boolean;
+}>`
   position: absolute;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
 
-  ${(props) => (props.$hidden ? "" : "& circle { transition: all 1s linear; }")}
+  @keyframes animate {
+    from {
+      opacity: 1;
+    }
+
+    to {
+      opacity: 0;
+    }
+  }
+
+  ${(props) =>
+    props.$duration === 0
+      ? ` & #circle2 { animation: animate 0.75s linear forwards }`
+      : ""}
 `;
+
+// ${(props) =>
+// props.$hidden ? "" : "& circle { transition: all 1s linear; }"}
 
 export default CircularProgress;
