1d9a42ed8c4b28494ca278ac3fd5f7c627da5ff0
MacJ13 <jacdim02@gmail.com>
2024-02-21 20:56:26 +0100

    Update Header padding

--- src/components/styles/Header.styled.ts
+++ src/components/styles/Header.styled.ts
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const StyledHeader = styled.header`
   position: relative;
-  padding: 1rem 0;
+  padding: 0.8rem 0;
   width: 100%;
 
   background-color: ${({ theme }) => theme.colors.white025};
